4ced34e0035ca87ff9e3455af04fec23ee26c5c9
Ravinder Bhalla <ravinder.bhalla@gmail.com>
2016-03-01 02:12:19 -0500

    This is the final commit and push to GIT. I have implemented a lot of my own optionals. Project is done

--- InstaBeast/CaptureViewController.swift
+++ InstaBeast/CaptureViewController.swift
@@ -161,27 +161,66 @@ class CaptureViewController: UIViewController, UINavigationControllerDelegate, U
     
     @IBAction func changeFilter(sender: AnyObject)
     {
+        let ac = UIAlertController(title: "Choose Filter", message: nil, preferredStyle: .ActionSheet)
+        ac.addAction(UIAlertAction(title: "CIBumpDistortion", style: .Default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIGaussianBlur", style: .Default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIPixellate", style: .Default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CISepiaTone", style: .Default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CITwirlDistortion", style: .Default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIUnsharpMask", style: .Default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIVignette", style: .Default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
+    }
+    
+    func setFilter(action: UIAlertAction!)
+    {
+        currentFilter = CIFilter(name: action.title!)
         
+        let beginImage = CIImage(image: picture.image!)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        
+        applyProcessing()
     }
     
     
     @IBAction func savePic(sender: AnyObject)
     {
+        UIImageWriteToSavedPhotosAlbum(picture.image!, self, "image:didFinishSavingWithError:contextInfo:", nil)
+    }
+    
+    func image(image: UIImage, didFinishSavingWithError error: NSError?, contextInfo: UnsafePointer<Void>)
+    {
+        if error == nil
+        {
+            let ac = UIAlertController(title: "Saved!", message: "Image has been saved to photo library", preferredStyle: UIAlertControllerStyle.Alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            presentViewController(ac, animated: true, completion: nil)
+            
+        }
+        else
+        {
+            let ac = UIAlertController(title: "Save error", message: error?.localizedDescription, preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            presentViewController(ac, animated: true, completion: nil)
+        }
         
     }
 
     @IBAction func intensityChanged(sender: AnyObject)
     {
         
-        
-        //applyProcessing();
-        
         applyProcessing();
     }
     
     func applyProcessing()
     {
-        currentFilter.setValue(intensity.value, forKey: kCIInputImageKey)
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey) }
+        if inputKeys.contains(kCIInputCenterKey) { currentFilter.setValue(CIVector(x: picture.image!.size.width / 2, y: picture.image!.size.height / 2), forKey: kCIInputCenterKey) }
         
         let cgimg = context.createCGImage(currentFilter.outputImage!, fromRect: currentFilter.outputImage!.extent)
         let processedImage = UIImage(CGImage: cgimg)
